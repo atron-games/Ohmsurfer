f3dd434c940adf5e1fcf469f2cd8a4ad290f0c03
atron-games <aaron.cain@edu.sait.ca>
2022-11-18 16:33:30 -0700

    full character controls
    added single & double tap input differentiation.
    
    added rigidbody physics for movement actions.
    
    added collision checks for ground & charged ground.
    
    added charge system and added charge cost conditions to player actions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,48 +4,101 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float tapThreshold;
+    //INPUT PARAMETERS
+    private float tapThreshold = 0.075f;
+    private float doubleMaxInterval = 0.1f;
+    private bool checkingForDouble = false;
+    private bool isDouble = false;
+
+    //PLAYER PARAMETERS
+    private Rigidbody2D playerRb;
     public float jumpForce;
     public float jetpackForce;
+    private bool isOnGround = false;
+    public float charge = 0;
 
-    private Rigidbody2D playerRb;
-
-    // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && checkingForDouble == false)
         {
-            StartCoroutine(InputCheck());
+            StartCoroutine(TapInput());
         }
+
+        Debug.Log(charge);
     }
 
-    IEnumerator InputCheck()
+    IEnumerator TapInput()
     {
+        //GIVE THE USER A MOMENT TO LET GO IF TAPPING
         yield return new WaitForSeconds(tapThreshold);
-        bool isOnGround = true;
 
-        if (Input.GetKey(KeyCode.Space) == false && isOnGround)
+        //IF THE USER LET GO (TAP)
+        if (Input.GetKey(KeyCode.Space) == false)
         {
-            Debug.Log("Jump!");
-            Jump();
+            //CHECK FOR DOUBLE TAP
+            checkingForDouble = true;
+            isDouble = false;
+            float t = 0;
+
+            while(t < doubleMaxInterval)
+            {
+                t += Time.deltaTime;
+               
+                //SHOOT IF IT IS A DOUBLE TAP
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Shoot();
+                    isDouble = true;
+                    break;
+                }
+                yield return t;
+            }
+
+            //JUMP IF IT'S NOT A DOUBLE AND YOU ARE ON THE GROUND
+            if (isOnGround && isDouble == false)
+            {
+                Jump();
+            }
+
+            //WAIT A LITTLE BIT SO WE DONT START FLOATING DURING THE DOUBLE TAP
+            yield return new WaitForSeconds(.1f);
+            checkingForDouble = false;
         }
 
-        while (Input.GetKey(KeyCode.Space))
+        //FLOAT IF THE BUTTON IS HELD DOWN
+        while (Input.GetKey(KeyCode.Space) && checkingForDouble == false)
         {
-            Debug.Log("Floating...");
             JetPack();
-
             yield return 0;
         }
 
     }
 
+    //COLLISION
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Electric")
+        {
+            ChargeUp();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOnGround = false;
+    }
+
+    //CHARACTER ACTIONS
     void Jump()
     {
         playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
@@ -53,6 +106,28 @@ public class PlayerController : MonoBehaviour
 
     void JetPack()
     {
-        playerRb.AddForce(Vector2.up * jetpackForce, ForceMode2D.Force);
+        if(charge > 0)
+        {
+            charge -= Time.deltaTime;
+            playerRb.AddForce(Vector2.up * jetpackForce, ForceMode2D.Force);
+        }
+    }
+
+    void Shoot()
+    {
+        if(charge >= 1)
+        {
+            charge = charge - 1;
+            Debug.Log("Pew Pew");
+        }
+    }
+
+    void ChargeUp()
+    {
+        if(charge <= 5)
+        {
+            charge += Time.deltaTime;
+            playerRb.WakeUp();
+        }
     }
 }
